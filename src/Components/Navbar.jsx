aa2a1dea3ab488b93f31ad7da475e5e2cc300bb8
Your_Name <musharafgujjar7711@gmail.com>
2024-09-30 11:43:39 +0500

    change

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,16 +12,21 @@ function Navbar() {
     <div className=' border relative md:flex md:items-center md:justify-between p-4 '>
       <div className='flex items-center justify-between'>
         <h1 className='capitalize font-bold text-blue-400'>logo</h1>
-        
-       
+
+
+
+
         <div className='md:hidden cursor-pointer' onClick={toggleMenu}>
-          <div className={`h-1 w-8 bg-blue-400 mb-1 transition-transform ${isOpen ? 'rotate-45 translate-y-2' : ''}`}></div>
-          <div className={`h-1 w-8 bg-blue-400 mb-1 transition-opacity ${isOpen ? 'opacity-0' : 'opacity-100'}`}></div>
-          <div className={`h-1 w-8 bg-blue-400 mb-1 transition-transform ${isOpen ? '-rotate-45 -translate-y-2' : ''}`}></div>
+          {isOpen ? (
+            <FaTimes className='text-blue-400 h-6 w-6' />
+          ) : (
+            <FaBars className='text-blue-400 h-6 w-6' />
+          )}
         </div>
+
       </div>
 
-      
+
       {isOpen && (
         <div className='absolute top-16 left-0 w-full h-60 bg-white border-t shadow-lg z-10 flex flex-col'>
           <ul className='flex flex-col items-start p-4'>
@@ -33,8 +39,8 @@ function Navbar() {
         </div>
       )}
 
-     
-      <ul className='hidden md:flex capitalize items-end gap-12'> 
+
+      <ul className='hidden md:flex capitalize items-end gap-12'>
         <li className='hover:text-sky-300 duration-100 cursor-pointer'>home</li>
         <li className='hover:text-sky-300 duration-100 cursor-pointer'>about us</li>
         <li className='hover:text-sky-300 duration-100 cursor-pointer'>blog</li>
